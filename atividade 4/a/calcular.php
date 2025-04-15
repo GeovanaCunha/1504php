c8ef7c1b15c79cdfedd9c3dd6cd5423c28498675
EmillyChristinny <emillychristinnyal@gmail.com>
2025-04-15 11:00:30 -0300

    atividade 4 - Emilly

---
+++ atividade 4/a/calcular.php
@@ -0,0 +1,12 @@
+<?php
+$num1 = $_POST["num1"];
+$num2 = $_POST["num2"];
+ 
+$min = min($num1, $num2);
+$max = max($num1, $num2);
+ 
+$aleatorio = rand($min, $max);
+ 
+echo "<h2>Número sorteado entre $min e $max: <strong>$aleatorio</strong></h2>";
+?>
+<br>
