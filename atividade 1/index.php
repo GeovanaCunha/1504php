8484c04f3d830b845dc0423cf4d73a520e28ceee
GeovanaCunha <geovanahcunha1109@gmail.com>
2025-04-15 09:32:55 -0300

    fazendo index

---
+++ atividade 1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Números</title>
+</head>
+<body>
+    <h1>clique no botao</h1>
+
+    <form method="get">
+       
+        <button type="submit">Mostrar</button>
+    </form>
+    </body>
+</html>
